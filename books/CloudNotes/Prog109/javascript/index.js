1245cce24ebb125157330e5088f77aa2f902b4c1
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2018-11-27 12:08:12 -0800

    send

---
+++ books/CloudNotes/Prog109/javascript/index.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+
+    const appearanceButton = document.getElementById('appearanceAction');
+
+    appearanceButton.onclick = () => {
+        const defaults = document.getElementById('default');
+        const basic = document.getElementById('basic');
+
+        disableStylesheet(defaults);
+        enableStylesheet(basic);
+    }
+}
+
+function enableStylesheet (node) {
+    node.rel = 'stylesheet';
+  }
+  
+function disableStylesheet (node) {
+    node.rel = 'alternate stylesheet';
+}
