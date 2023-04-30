cc9383c8e6719926404131a33d3fd442904c4a64
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-04-30 10:14:30 -0700

    install bundler

--- scripts/3-setup-jekyll.sh
+++ scripts/3-setup-jekyll.sh
@@ -4,6 +4,7 @@
 LIGHT_GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
+ln -s ${SETUP_LINUXBOX}/.my_bash_aliases ~/.my_bash_aliases
 sudo apt-get install ruby-full build-essential zlib1g-dev -y
 echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
