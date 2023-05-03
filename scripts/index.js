def18ff3a73d34f99936e690dae495afff910bad
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-03 13:15:37 -0700

    dead code

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,3 @@ async function main() {
 }
 
 main();
-
-
-
-/* import { createJekyllPost } from "elven-jekyll-post/utils";
-
-import { input } from '@inquirer/prompts';
-
-
-async function run() {
-    const answer = await input({ message: 'Enter base post name:' });
-
-    createJekyllPost(answer);
-}
-
-await run();
-
-export default run; */
