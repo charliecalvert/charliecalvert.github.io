7bbc8b57308d7c93ba27c2089793f16af4bf3d48
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-09 13:29:46 -0700

    scripts

--- scripts/copy-cloud.sh
+++ scripts/copy-cloud.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Copy files to cloud directory
+# CLOUDNOTES="$HOME/Git/CloudNotes"
+SOURCE_DIR="$CLOUDNOTES/elvenware/development/cloud/"
+DEST_DIR="$HOME/Git/charliecalvert.github.io/_posts/elvenware/development/cloud"
 
-SOURCE_DIR="$HOME/Git/CloudNotes/development/cloud/"
-DEST_DIR="$HOME/Git/charliecalvert.github.io/_posts/development/cloud"
+# rsync -avru --delete --dry-run --include="*/" --include="*.md" --exclude="*" ${SOURCE_DIR} ${DEST_DIR}
+rsync -avru --delete --include="*/" --include="*.md" --exclude="*" ${SOURCE_DIR} ${DEST_DIR}
 
-rsync -avu --delete ${SOURCE_DIR} ${DEST_DIR}
