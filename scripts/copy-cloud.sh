d8f109b9785dfb48b17dbaab5b6b966a3c2ae75f
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-09 07:39:34 -0700

    work in progress

--- scripts/copy-cloud.sh
+++ scripts/copy-cloud.sh
@@ -5,4 +5,4 @@
 SOURCE_DIR="$HOME/Git/CloudNotes/development/cloud/"
 DEST_DIR="$HOME/Git/charliecalvert.github.io/_posts/development/cloud"
 
-rsync -avu --delete SOURCE_DIR DEST_DIR
+rsync -avu --delete ${SOURCE_DIR} ${DEST_DIR}
