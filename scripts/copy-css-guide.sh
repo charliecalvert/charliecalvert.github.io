27ee42cd60be8f82e45fec2388f57c4e82241445
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-09 18:55:33 -0700

    copyguide

--- scripts/copy-css-guide.sh
+++ scripts/copy-css-guide.sh
@@ -2,7 +2,7 @@
 
 # Copy files to css-guide directory
 SOURCE_DIR="$CLOUDNOTES/elvenware/development/web/CssGuide/"
-DEST_DIR="$HOME/Git/charliecalvert.github.io/elves/css-guide"
+DEST_DIR="$HOME/Git/charliecalvert.github.io/css-guide"
 
 # rsync -avru --delete --dry-run --include="*/" --include="*.md" --exclude="*" ${SOURCE_DIR} ${DEST_DIR}
 rsync -avru --delete --include="*/" --include="*.md" --exclude="*" ${SOURCE_DIR} ${DEST_DIR}
