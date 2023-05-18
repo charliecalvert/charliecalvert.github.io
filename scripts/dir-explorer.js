d0c1ee9c326e90bd7767d4acff4a3d4d083c214d
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-18 12:13:21 -0700

    wip test

--- scripts/dir-explorer.js
+++ scripts/dir-explorer.js
@@ -7,6 +7,19 @@ const debugLink = createDebugMessages('lib:dir-explorer:link');
 debug('process.env.CHAIO', process.env.CHAIO);
 const CHAIO = process.env.CHAIO;
 debug('CHAIO', CHAIO);
+
+function writeCopyScript(directoryName, scriptName, index) {
+    const file = `#!/bin/bash
+
+    # Copy files to css-guide directory
+    SOURCE_DIR="$CLOUDNOTES/assignments/${directoryName}/"
+    DEST_DIR=${directoryName};
+
+    rsync -avru --delete --include="*/" --include="*.md" --exclude="*" ${SOURCE_DIR} ${DEST_DIR}
+    `;
+    writeFileSync(`%{lastItem}.sh`, file);
+}
+
 async function listFiles() {
     const directoryLinks = [];
     debug('listFiles called');
@@ -27,6 +40,7 @@ async function listFiles() {
         const markdownLink = `[${lastItem}](${remainder})`;
         debugLink('markdownLink -', markdownLink);
         directoryLinks.push(`- ${markdownLink}`);
+        writeCopyScript(fileName, lastItem, index);
         return index;
     });
     writeFileSync(`${process.env.CHAIO}/assignments/all-links.markdown`, directoryLinks.join('\n'));
