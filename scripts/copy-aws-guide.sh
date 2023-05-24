5a084fc3c31279d7ac12c6f1a4610480652dd0ed
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-23 17:33:29 -0700

    Clean up the AWS guide

--- scripts/copy-aws-guide.sh
+++ scripts/copy-aws-guide.sh
@@ -2,8 +2,8 @@
 
 # Copy files to cloud directory
 # CLOUDNOTES="$HOME/Git/CloudNotes"
-SOURCE_DIR="$CLOUDNOTES/Assignments/Firebase/"
-DEST_DIR="${CHARLIE_IO}/elves/_firebase-guide"
+SOURCE_DIR="$CLOUDNOTES/Assignments/Aws/"
+DEST_DIR="${CHARLIE_IO}/elves/_aws-guide"
 
 # rsync -avru --delete --dry-run --include="*/" --include="*.md" --exclude="*" ${SOURCE_DIR} ${DEST_DIR}
 rsync -avru --delete --include="*/" --include="*.md" --exclude="*" ${SOURCE_DIR} ${DEST_DIR}
