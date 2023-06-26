017958d82ea1ea6b586f3dc401a4093fc6097c3b
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-06-25 17:21:40 -0700

    better: directory changing

--- scripts/do-all.sh
+++ scripts/do-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cn # Go to CloudNotes
+cd /home/ubuntu/Git/CloudNotes # Go to CloudNotes
 npm run InsertFrontMatterNotWorking2 # Insert front matter
-chaio # Go to charliecalvert.github.io
+cd ${CHAIO} # Go to charliecalvert.github.io
 npm run copy # Copy files from CloudNotes to charliecalvert.github.io
 ./create_site.sh # Create Jekyll _site
 cd scripts/
